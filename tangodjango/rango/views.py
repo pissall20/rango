29268a7eee7eaa38d02fcd8aa19ada9c85a052de
pissall20 <pisalsiddhesh@gmail.com>
2017-06-10 00:50:10 +0530

    Second commit, added template folder.

--- tangodjango/rango/views.py
+++ tangodjango/rango/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hey there, here is the Rango App developed by Siddhesh")
+    return HttpResponse("Hey there, here is the Rango App developed by Siddhesh <br /><a href = '/rango/about/'> About </a>")
+
+
+def about(request):
+    return HttpResponse("Here is the about page. <br /><a href = '/rango/'> Index </a>")
+
