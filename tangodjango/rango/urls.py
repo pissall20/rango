6ec2b6721f26090211971fe3c5f1358fb5e5b992
pissall20 <pisalsiddhesh@gmail.com>
2017-06-13 12:56:16 +0530

    Created the functionality for users to register(with profile pictures too)

--- tangodjango/rango/urls.py
+++ tangodjango/rango/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
     url(r'^add_category/$', views.add_category, name='add_category'),
+    url(r'^register/$', views.register, name='register'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
 ]
