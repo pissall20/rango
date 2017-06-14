1f3a78b85fef83b89775e5a368ed1af453edde1d
pissall20 <pisalsiddhesh@gmail.com>
2017-06-14 12:39:24 +0530

    Added bootstrap and jquery. The look of the site is not ugly anymore.

--- tangodjango/rango/forms.py
+++ tangodjango/rango/forms.py
@@ -33,7 +33,9 @@ class PageForm(forms.ModelForm):
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(help_text="Please enter a username")
+    email = forms.EmailField(help_text="Please enter a valid email ID")
+    password = forms.CharField(widget=forms.PasswordInput, help_text="Please enter a password")
 
     class Meta:
         model = User
@@ -41,6 +43,8 @@ class UserForm(forms.ModelForm):
 
 
 class UserProfileForm(forms.ModelForm):
+    website = forms.URLField(help_text="Please enter a valid URL with http:// prefix")
+    picture = forms.ImageField(help_text="Select a profile image to upload")
     class Meta:
         model = UserProfile
         fields = ('website', 'picture')
